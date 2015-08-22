9966e800135f99e073129615e9745d1472f1c047
imarin2 <imarin@noreply>
2015-08-22 17:42:39 +0200

    Typo correction

--- addfinishingpass.sh
+++ addfinishingpass.sh
@@ -4,15 +4,14 @@
 # $2 => finishing extra depth in mm
 # your output is in a file with the same name but ending with _finishpath.gcode
 
-finishpathfile=`echo $1 | sed 's/.gcode/.finishpath/g'`
-outputfile=`echo $1 | sed 's/.gcode/_finishpath.gcode/g'`
+finishpassfile=`echo $1 | sed 's/.gcode/.finishpass/g'`
+outputfile=`echo $1 | sed 's/.gcode/_finishpass.gcode/g'`
 
-cat $1 | sed -n '1,/G4 S3/!p' | sed -n '/G4 S5/,$!p' | sed -r 's/(G1 *Z-)([0-9]+\.*[0-9]*)/ echo \1`echo "x="\2"+'"$2"';if(x<1) print 0; x" | bc` /ge'>$finishpathfile
+cat $1 | sed -n '1,/G4 S3/!p' | sed -n '/G4 S5/,$!p' | sed -r 's/(G1 *Z-)([0-9]+\.*[0-9]*)/ echo \1`echo "x="\2"+'"$2"';if(x<1) print 0; x" | bc` /ge'>$finishpassfile
 
-awk -v cmd="cat $finishpathfile" '/G4 S5/ { system ( cmd ) } { print; } \' $1 > $outputfile
+awk -v cmd="cat $finishpassfile" '/G4 S5/ { system ( cmd ) } { print; } \' $1 > $outputfile
 
-rm $finishpathfile
-#less $finishpadfile
+rm $finishpassfile
 
 
 
